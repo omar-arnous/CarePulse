4368f815a1ce54c53b5b28aa70d24d86655a2438
omar-arnous <omar99.arnous@gmail.com>
2024-09-09 10:26:27 +0300

    initialize the appwrite logic and connectivity

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { userFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -42,13 +43,13 @@ const PatientForm = () => {
 
     try {
       console.log({ name, email, phone });
-      //   const userData = {
-      //     name,
-      //     email,
-      //     phone,
-      //   };
-      //   const user = await createUser(userData);
-      //   if (user) router.push(`/patients/${user.$id}/register`);
+      const userData = {
+        name,
+        email,
+        phone,
+      };
+      const user = await createUser(userData);
+      if (user) router.push(`/patients/${user.$id}/register`);
     } catch (error) {
       console.log(error);
     }
